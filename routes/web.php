b3354e922c5ed539a77c24ae439975b65e35734d
jagaimo3310 <sawashin2003310@gmail.com>
2025-02-09 04:13:53 +0000

    02-01 javascript answer

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use App\Http\Controllers\PostController;
 
 
 Route::get('/', [PostController::class, 'index']);
+//カリキュラム解答02-1-JavaScript
+Route::get('/JavaScript', [PostController::class, 'JavaScript']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);

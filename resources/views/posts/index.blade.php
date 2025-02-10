448ae6b8d7f451abe6b8a23ed49ffb2aa5497312
jagaimo3310 <sawashin2003310@gmail.com>
2025-02-10 07:11:51 +0000

    correction

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,48 +1,61 @@
 <!DOCTYPE html>
 <html lang="ja">
-<x-app-layout>
-    <x-slot name="header">
 
-        <head>
-            <meta charset="utf-8">
-            <title>Blog</title>
-            <!-- Fonts -->
-            <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        </head>
-    </x-slot>
+<head>
+    <meta charset="utf-8">
+    <title>Blog</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+</head>
 
-    <body>
-        <a href='/posts/create'>create</a>
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            @foreach ($posts as $post)
-            <div class='post'>
-                <a href="/posts/{{$post->id}}" class='title'>{{ $post->title }}<a><br>
-                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                        <p class='body'>{{ $post->body }}</p>
-            </div>
-            @endforeach
+
+<body>
+    <a href='/posts/create'>create</a>
+    <h1>Blog Name</h1>
+    <div class='posts'>
+        @foreach ($posts as $post)
+        <div class='post'>
+            <a href="/posts/{{$post->id}}" class='title'>{{ $post->title }}<a><br>
+                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                    <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{$post->id}})">delete</button>
+                    </form>
         </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+        @endforeach
+    </div>
+    <div class='paginate'>
+        {{ $posts->links() }}
+    </div>
 
-        <div class='test'>
-            <a class='Javascript' href='/JavaScript'>Javascript</a>
-        </div>
+    <div class='test'>
+        <a class='Javascript' href='/JavaScript'>Javascript</a>
+    </div>
 
+    <div>
+        @foreach($questions as $question)
         <div>
-            @foreach($questions as $question)
-            <div>
-                <a href="https://teratail.com/questions/{{ $question['id'] }}">{{$question['title']}}</a>
-            </div>
-            @endforeach
+            <a href="https://teratail.com/questions/{{ $question['id'] }}">{{$question['title']}}</a>
         </div>
+        @endforeach
+    </div>
 
-        <div class='userName'>
-            <p>ログインユーザー:{{ Auth::user()->name }}</p>
-        </div>
-    </body>
-</x-app-layout>
+    <div class='userName'>
+        <p>ログインユーザー:{{ Auth::user()->name }}</p>
+    </div>
+</body>
+
+
+<script>
+    function deletePost(id) {
+        'use strict'
+
+        if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+            document.getElementById(`form_${id}`).submit();
+        }
+    }
+</script>
 
 </html>

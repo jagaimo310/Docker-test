dd9f12ad74c2477271d27c4dcbfc3b3bedbab1c6
jagaimo3310 <sawashin2003310@gmail.com>
2025-04-07 08:02:27 +0000

    saveRecipe

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+//日付操作ライブラリ
+use Carbon\Carbon;
 
 class RecipeController extends Controller
 {
@@ -145,6 +148,58 @@ class RecipeController extends Controller
         // $ingredient->save();
 
         //ここは本番では処理を書き換えること
-        return dd("食材を保存しました", $protein, $fat, $carbohydrate);
+        return dd("食材を保存しました", $nutrients, $protein, $fat, $carbohydrate);
+    }
+
+    public function saveRecipe(Request $request, Recipe $recipe, RecipeIngredient $recipeingredient, Record $record)
+    {
+        //inputに複数データを入れる場合の処理を確認
+        $ids = $request['ingredient_id'];
+        $quantities = $request['quantitiy'];
+        $carbohydrate = 0;
+        $protein = 0;
+        $fat = 0;
+        $calorie = 0;
+        $names = [];
+
+        for ($i = 0; $i < count($ids); $i++) {
+            $names[$i] = $ids[$i]->ingredients('name');
+            $carbohydrate += (int)$ids[$i]->ingredients('carbohydrate') * $quantities[$i] / 100;
+            $protein += (int)$ids[$i]->ingredients('protein') * $quantities[$i] / 100;
+            $fat += (int)$ids[$i]->ingredients('fat') * $quantities[$i] / 100;
+        }
+
+        $calorie += 4 / 100 * $carbohydrate;
+        $calorie += 4 / 100 * $protein;
+        $calorie += 9 / 100 * $fat;
+
+        //recipesテーブルの保存処理 name、descriotin、instructionのform名は'recipe[]'となっているものとする
+        $inputRecipe = $request['recipe'];
+        $recipe->fill($inputRecipe);
+        $recipe->calorie = $calorie;
+        $recipe->protein = $protein;
+        $recipe->fat = $fat;
+        $recipe->carbohydrate = $carbohydrate;
+        $recipe->save();
+
+        //レシピのidを使ってrecipe_ingredientsテーブルの保存処理
+        for ($i = 0; $i < count($ids); $i++) {
+            $recipeingredient->recipe_id = $recipe->id;
+            $recipeingredient->ingredient_id = $ids[$i];
+            $recipeingredient->name = $names[$i];
+            $recipeingredient->quantity = $quantities[$i];
+            $recipeingredient->save();
+        }
+
+
+        //recordsも同時に保存処理
+        $userId = Auth::id();
+        $record->user_id = $userId;
+        $record->recipe_id = $recipe->id;
+        $record->meal_date = Carbon::today()->toDateString();
+        $record->save();
+
+        //リダイレクトを行う
+
     }
 }
